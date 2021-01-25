cd50d55f4b35e3b5e1fea7257e472ce5beeea7ef
quentin <cartronq@gmail.com>
2021-01-25 15:50:05 +0100

    Update donjunix-deck.py

--- donjunix-deck.py
+++ donjunix-deck.py
@@ -1,6 +1,5 @@
 
 import itertools, random
-
 deck=list(itertools.product(["detruire-monstre","stocker-objet","deposer-objet","recule-d'une-case","retour-au-depart"]))
           
 random.shuffle(deck)
@@ -8,9 +7,10 @@ random.shuffle(deck)
 print("vous avez obtenu:")
 for i in range(1):
     print(deck[i][0])
-
-pile=[]
-pile.append(deck[i][0])
-
-print(pile)
-
+    r=g+1
+    break
+p=[]
+while r > 20:
+    p.append(deck[i][0])
+    return r
+    print(p)
